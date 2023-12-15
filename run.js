1d8f2e9d324afd9559806db467cd6a951a89d572
Udin Saputra <132532759+udinsp@users.noreply.github.com>
2023-12-15 16:03:34 +0700

    Change from cheerio to puppeteer

--- run.js
+++ run.js
@@ -1,55 +1,48 @@
+const puppeteer = require('puppeteer');
 const axios = require('axios');
-const cheerio = require('cheerio');
 const moment = require('moment-timezone');
+
+let browser; 
 
 // Function to get a timestamp in the [HH:mm DD/MM/YYYY] format in the Jakarta timezone
 function getTimestamp() {
   const currentDate = moment().tz('Asia/Jakarta');
-  const hours = currentDate.format('HH');
-  const minutes = currentDate.format('mm');
-  const day = currentDate.format('DD');
-  const month = currentDate.format('MM');
-  const year = currentDate.format('YYYY');
-
-  return `[${hours}:${minutes} ${day}/${month}/${year}]`;
+  return `[${currentDate.format('HH:mm DD/MM/YYYY')}]`;
 }
 
 // Function to check if a user is currently live streaming
 async function checkLiveStatus(username) {
   try {
+    const page = await browser.newPage();
+
     const profileUrl = `https://www.tiktok.com/@${username}`;
-    const response = await axios.get(profileUrl);
-    const html = response.data;
+    await page.goto(profileUrl);
 
-    // Using cheerio to parse HTML
-    const $ = cheerio.load(html);
-    const liveLink = $('a[target="tiktok_live_view_window"]').attr('href');
+    await page.waitForSelector('a[target="tiktok_live_view_window"]', { timeout: 60000 });
 
-    // If there's a live link, the user is live streaming
-    if (liveLink) {
-      return true;
-    } else {
-      return false;
-    }
+    const liveLink = await page.$eval('a[target="tiktok_live_view_window"]', (link) => link.href);
+
+    await page.close(); 
+
+    return !!liveLink;
   } catch (error) {
-    throw new Error(`${getTimestamp()} Failed to check live streaming status: ${error.message}`);
+    return false;
   }
 }
 
 // Function to send notifications to Telegram
-async function sendTelegramNotification(message) {
+async function sendTelegramNotification(username) {
   try {
     const telegramApiUrl = 'https://api.telegram.org/xxxxxxx/sendMessage'; // Telegram Token
     const chatId = 'xxxxxxx'; // Chat ID
 
     const response = await axios.post(telegramApiUrl, {
       chat_id: chatId,
-      text: message,
+      text: `${username} is currently live streaming\n\nhttps://www.tiktok.com/@${username}/live`,
     });
 
-    // Checking the notification sending status
     if (response.status === 200) {
-      console.log(`${getTimestamp()} Notification sent to Telegram successfully.`);
+      console.log(`${getTimestamp()} User @${username} is live, notification sent to Telegram.`);
     } else {
       throw new Error(`${getTimestamp()} Failed to send notification to Telegram.`);
     }
@@ -70,8 +63,7 @@ async function checkLiveStatusAndSendNotification(username) {
       const currentTime = new Date();
 
       if (!lastNotificationTimes[username] || currentTime - lastNotificationTimes[username] >= 3 * 60 * 60 * 1000) {
-        const message = `${username} is currently live streaming\n\nhttps://www.tiktok.com/@${username}/live`;
-        await sendTelegramNotification(message);
+        await sendTelegramNotification(username);
         lastNotificationTimes[username] = currentTime;
       } else {
         console.log(`${getTimestamp()} User @${username} is live streaming, but notifications are only sent every 3 hours.`);
@@ -80,21 +72,41 @@ async function checkLiveStatusAndSendNotification(username) {
       console.log(`${getTimestamp()} User @${username} is not currently live streaming.`);
     }
   } catch (error) {
-    console.error(`${getTimestamp()} Failed to check live streaming status and send notifications for user @${username}:`, error.message);
+    console.error(`${getTimestamp()} Failed to check live streaming status and send notifications for @${username}: ${error.message}`);
   }
 }
 
 // List of usernames to be checked
-const usernames = ['jessijkt48', 'jkt48.official']; 
+const usernames = ['jessijkt48', 'jkt48.official'];
 
-// Check live streaming status and send notifications right now for each username
-usernames.forEach((username) => {
-  checkLiveStatusAndSendNotification(username);
+// Set a maximum of 15 listeners (or as needed)
+process.setMaxListeners(15);
+
+// Close the browser when the program is stopped
+process.on('SIGINT', async () => {
+  if (browser) {
+    await browser.close();
+  }
+  process.exit(0);
 });
 
-// Check live streaming status and send notifications every 3 minutes for each username
-setInterval(() => {
-  usernames.forEach((username) => {
-    checkLiveStatusAndSendNotification(username);
+// Open browser
+(async () => {
+  browser = await puppeteer.launch({
+    headless: 'new',
+    defaultViewport: null,
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-notifications'],
   });
-}, 3 * 60 * 1000);
+
+  // Check live streaming status and send notifications right now for each username
+  for (const username of usernames) {
+    await checkLiveStatusAndSendNotification(username);
+  }
+
+  // Check live streaming status and send notifications every 3 minutes for each username
+  setInterval(async () => {
+    for (const username of usernames) {
+      await checkLiveStatusAndSendNotification(username);
+    }
+  }, 3 * 60 * 1000);
+})();
